23585f1a6cbfcbd72e8ccd2680743683a65810e2
Renan Marangon <maracunha@gmail.com>
2023-12-27 09:26:11 -0300

    chore: Add router and redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './pages/Login';
 import Suppliers from './pages/Suppliers';
-import SuppliersDetail from './pages/SuppliersDetail';
+import RequireAuth from './components/RequireAuth';
 
 function App() {
     return (
         <main>
             <Header />
 
-            <Login />
-            <Suppliers />
-            <SuppliersDetail />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="*"
+                    element={
+                        <RequireAuth>
+                            <Suppliers />
+                        </RequireAuth>
+                    }
+                />
+            </Routes>
         </main>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import { store } from './store';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
 );
