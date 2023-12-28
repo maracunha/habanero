05dd6bf3ac297a390edc633e7e9ebf971cb65de4
Renan Marangon <maracunha@gmail.com>
2023-12-28 17:56:52 -0300

    feat: Add 404 page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
+
+import styles from './styles.module.scss';
+
 function NotFound() {
-    // TODO not found poge
-    return <h2>404</h2>;
+    return (
+        <div className={styles.error}>
+            <h3>Oops! There's nothing here</h3>
+            <p>
+                <Link to="/">Back</Link>
+            </p>
+        </div>
+    );
 }
 
 export default NotFound;
